bab217b6a53d9416c41a28d554ed2e37209239a9
misha <misha@3dcf240c-a488-453e-83cb-a6019b294898>
2012-05-03 15:58:32 +0000

    Passport and PassportForm refactored
    git-svn-id: svn://192.168.0.55/repo@25 3dcf240c-a488-453e-83cb-a6019b294898

--- voyanga/protected/models/Passport.php
+++ voyanga/protected/models/Passport.php
@@ -5,41 +5,61 @@
  * @author oleg
  *
  */
-class Passport extends CActiveRecord {
-    public $id;
-    public $first_name;
-    public $last_name;
-    public $number;
-    public $birthday;
-    public $series;
+
+/**
+ * This is the model class for table "passport".
+ *
+ * The followings are the available columns in table 'passport':
+ * @property integer $id
+ * @property string $firstName
+ * @property string $lastName
+ * @property string $number
+ * @property string $birthday
+ * @property string $series
+ * @property integer $documentTypeId
+ * @property integer $countryId
+ */
+
+//todo: alter birthday to date
+//todo: add gender_id to db
+//todo: add expiration to db
+//todo: country_id as foreign key
+class Passport extends CActiveRecord
+{
     /*
-     * documentTypeId values:
-     * 1 - Passport RF
-     * 2 - Passport other country
-     * 3 - Zagran
-     */
-    public $document_type_id;
-    public $expiration;
-    public $country_id;
-    public $gender_id;
-    
-    public static function model( $className = __CLASS__ ) {
-        return parent::model( $className );
+    * documentTypeId values:
+    * 1 - Passport RF
+    * 2 - Passport other country
+    * 3 - Zagran
+    */
+    const TYPE_RF = 1;
+    const TYPE_OTHER = 2;
+    const TYPE_INTERNATIONAL = 3;
+
+    //public $expiration;
+    //public $gender_id;
+
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
     }
-    
-    public function rules() {
+
+    public function rules()
+    {
         return array(
-                // name, email, subject and body are required
-                array(
-                        'first_name, last_name, number, birthday, document_type_id, gender_id', 
-                        'required' ) );
+            // name, email, subject and body are required
+            array(
+                'first_name, last_name, number, birthday, document_type_id, gender_id',
+                'required'));
     }
-    
-    public function tableName() {
+
+    public function tableName()
+    {
         return 'passport';
     }
-    
-    public function checkValid(){
+
+    public function checkValid()
+    {
         return true;
     }
 }
--- voyanga/protected/models/PassportForm.php
+++ voyanga/protected/models/PassportForm.php
@@ -5,47 +5,50 @@
  * @author oleg
  *
  */
-class PassportForm extends CFormModel {
+class PassportForm extends CFormModel
+{
     public $id;
-    public $first_name;
-    public $last_name;
+    public $firstName;
+    public $lastName;
     public $number;
     public $birthday;
     public $series;
-    public $document_type_id;
-    public $country_id;
-    public $gender_id;
-    
+    public $documentTypeId;
+    public $countryId;
+    public $genderId;
+
     /**
      * Declares the validation rules.
      */
-    public function rules() {
+    public function rules()
+    {
         return array(
-                // first_name, last_name, number, birthday, document_type_id, gender_id are required
-                array(
-                        'first_name, last_name, number, birthday, document_type_id, gender_id', 
-                        'required' ), 
-                // email has to be a valid birthday format
-                array(
-                        'birthday', 
-                        'date','format'=>'dd.MM.yyyy' ) );
+            // first_name, last_name, number, birthday, document_type_id, gender_id are required
+            array(
+                'first_name, last_name, number, birthday, document_type_id, gender_id',
+                'required'),
+            // email has to be a valid birthday format
+            array(
+                'birthday',
+                'date', 'format' => 'dd.MM.yyyy'));
     }
-    
+
     /**
      * Declares customized attribute labels.
      * If not declared here, an attribute would have a label that is
      * the same as its name with the first letter in upper case.
      */
-    public function attributeLabels() {
+    public function attributeLabels()
+    {
         return array(
-                'verifyCode' => 'Verification Code', 
-                'first_name' => 'Имя', 
-                'last_name' => 'Фамилия', 
-                'number' => 'Номер документа', 
-                'birthday' => 'Дата рождения', 
-                'document_type_id' => 'Тип документа', 
-                'gender_id' => 'Пол', 
-                'series' => 'Серия документа', 
-                'country_id' => 'Гражданство' );
+            'verifyCode' => 'Verification Code',
+            'first_name' => 'Имя',
+            'last_name' => 'Фамилия',
+            'number' => 'Номер документа',
+            'birthday' => 'Дата рождения',
+            'document_type_id' => 'Тип документа',
+            'gender_id' => 'Пол',
+            'series' => 'Серия документа',
+            'country_id' => 'Гражданство');
     }
 }
