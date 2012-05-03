e7835d82e087d0d464a3e6f9057576f879a7195f
misha <misha@3dcf240c-a488-453e-83cb-a6019b294898>
2012-05-03 16:48:54 +0000

    FlightVoyageStack partially done
    git-svn-id: svn://192.168.0.55/repo@31 3dcf240c-a488-453e-83cb-a6019b294898

--- voyanga/protected/models/FlightVoyageStack.php
+++ voyanga/protected/models/FlightVoyageStack.php
@@ -5,116 +5,134 @@
  * @author oleg
  *
  */
-class FlightVoyageStack {
-    public $aFlightVoyages = array();
-    public $aFilterValues = array();
-    public static $iToTop;
-    
-    public $iBestMask = 0; // bitwise mask 0b001 - Best price, 0b010 - best recommended, 0b100 best speed
-    
+class FlightVoyageStack
+{
+    public $flightVoyages = array();
+    public $filterValues = array();
+    public static $toTop;
 
-    public function __construct( $aParams = NULL ) {
-        if ( $aParams ) {
-            $this->aAirportsFrom = array(
-                    array(), 
-                    array() );
-            $this->aAirportsTo = array(
-                    array(), 
-                    array() );
-            $this->aTimePeriodFrom = array(
-                    array(), 
-                    array() );
-            $this->aTimePeriodTo = array(
-                    array(), 
-                    array() );
-            $this->aAirlines = array();
-            $this->aTransits = array();
-            $this->iBestTime = 0;
-            $this->iBestPrice = 0;
-            $this->iBestPriceTime = 0;
-            
-            if ( $aParams['aFlights'] ) {
-                
+    public $bestMask = 0; // bitwise mask 0b001 - Best price, 0b010 - best recommended, 0b100 best speed
+
+
+    public function __construct($params = NULL)
+    {
+        if ($params)
+        {
+            $this->airportsFrom = array(
+                array(),
+                array());
+            $this->airportsTo = array(
+                array(),
+                array());
+            $this->timePeriodFrom = array(
+                array(),
+                array());
+            $this->timePeriodTo = array(
+                array(),
+                array());
+            $this->airlines = array();
+            $this->transits = array();
+            $this->bestTime = 0;
+            $this->bestPrice = 0;
+            $this->bestPriceTime = 0;
+
+            //todo: refactor here
+            if ($params['aFlights'])
+            {
                 $bParamsNeedInit = true;
-                foreach( $aParams['aFlights'] as $oFlightParams ) {
-                    $oFlightVoyage = new FlightVoyage( $oFlightParams );
+                foreach ($params['aFlights'] as $oFlightParams)
+                {
+                    $oFlightVoyage = new FlightVoyage($oFlightParams);
                     $bNeedSave = TRUE;
-                    if ( $bNeedSave ) {
+                    if ($bNeedSave)
+                    {
                         //If Voyage don't filtered, add to stack
                         $this->aFlightVoyages[] = $oFlightVoyage;
-                        
+
                         $iFullDuration = $oFlightVoyage->getFullDuration();
-                        if ( $bParamsNeedInit ) {
+                        if ($bParamsNeedInit)
+                        {
                             //initializing best params
                             $bParamsNeedInit = false;
-                            $this->iBestPrice = $oFlightVoyage->price;
-                            $this->iBestTime = $iFullDuration;
-                            $this->iBestTimeInd = count( $this->aFlightVoyages ) - 1;
+                            $this->bestPrice = $oFlightVoyage->price;
+                            $this->bestTime = $iFullDuration;
+                            $this->iBestTimeInd = count($this->aFlightVoyages) - 1;
                             $this->iBestPriceInd = $this->iBestTimeInd;
                         }
-                        if ( $this->iBestPrice > $oFlightVoyage->price ) {
+                        if ($this->bestPrice > $oFlightVoyage->price)
+                        {
                             //update best price params 
-                            $this->iBestPrice = $oFlightVoyage->price;
-                            $this->iBestPriceInd = count( $this->aFlightVoyages ) - 1;
+                            $this->bestPrice = $oFlightVoyage->price;
+                            $this->iBestPriceInd = count($this->aFlightVoyages) - 1;
                         }
-                        if ( $this->iBestTime > $iFullDuration ) {
+                        if ($this->bestTime > $iFullDuration)
+                        {
                             //update best time params
-                            $this->iBestTime = $iFullDuration;
-                            $this->iBestTimeInd = count( $this->aFlightVoyages ) - 1;
+                            $this->bestTime = $iFullDuration;
+                            $this->iBestTimeInd = count($this->aFlightVoyages) - 1;
                         }
                     }
                 }
-                
+
                 $bParamsNeedInit = true;
                 //find best pricetime params
-                foreach( $this->aFlightVoyages as $iInd => $oFlightVoyage ) {
+                foreach ($this->aFlightVoyages as $iInd => $oFlightVoyage)
+                {
                     $iFullDuration = $oFlightVoyage->getFullDuration();
-                    $iParamsFactor = intval( ( $oFlightVoyage->price / $this->iBestPrice ) * Yii::app()->params['flight_price_factor'] ) + intval( ( $iFullDuration / $this->iBestTime ) * Yii::app()->params['flight_time_factor'] );
-                    
-                    if ( $bParamsNeedInit ) {
+                    $iParamsFactor = intval(($oFlightVoyage->price / $this->bestPrice) * Yii::app()->params['flight_price_factor']) + intval(($iFullDuration / $this->bestTime) * Yii::app()->params['flight_time_factor']);
+
+                    if ($bParamsNeedInit)
+                    {
                         $bParamsNeedInit = false;
-                        $this->iBestPriceTime = $iParamsFactor;
+                        $this->bestPriceTime = $iParamsFactor;
                     }
-                    if ( $this->iBestPriceTime > $iParamsFactor ) {
-                        $this->iBestPriceTime = $iParamsFactor;
+                    if ($this->bestPriceTime > $iParamsFactor)
+                    {
+                        $this->bestPriceTime = $iParamsFactor;
                         $this->iBestPriceTimeInd = $iInd;
                     }
                 }
             }
         }
     }
-    
+
     /**
      * addFlightVoyage
      * Add FlightVoyage object to this FlightVoyageStack
      * @param FlightVoyage $oFlightVoyage
      */
-    public function addFlightVoyage( FlightVoyage $oFlightVoyage ) {
+    public function addFlightVoyage(FlightVoyage $oFlightVoyage)
+    {
         $this->aFlightVoyages[] = $oFlightVoyage;
-        $this->iBestMask |= $oFlightVoyage->iBestMask;
+        $this->bestMask |= $oFlightVoyage->iBestMask;
     }
-    
-    public function setAttributes( $values ) {
-        foreach( $values as $name => $value ) {
+
+    public function setAttributes($values)
+    {
+        foreach ($values as $name => $value)
+        {
             $this->$name = $value;
         }
     }
-    
+
     /**
      * Function for sorting by uksort
      * @param $a
      * @param $b
      */
-    private static function compare_array( $a, $b ) {
-        if ( $a < $b ) {
+    private static function compare_array($a, $b)
+    {
+        if ($a < $b)
+        {
             return -1;
-        } elseif ( $a > $b ) {
+        } elseif ($a > $b)
+        {
             return 1;
         }
-        
+
         return 0;
     }
-    
+
     /**
      * groypBy method
      * Group internal FlightVoyage elements, and return array of FlightVoyageStack elements
@@ -123,25 +141,29 @@ class FlightVoyageStack {
      * @param integer $iFlightIndex - index of Flight in FlightVoyage
      * @return array of FlightVoyageStack
      */
-    public function groupBy( $sKey, $iToTop = NULL, $iFlightIndex = FALSE ) {
+    public function groupBy($sKey, $iToTop = NULL, $iFlightIndex = FALSE)
+    {
         $aVariantsStacks = array();
-        
-        foreach( $this->aFlightVoyages as $oFlihtVoyage ) {
-            switch ( $sKey ) {
+
+        foreach ($this->aFlightVoyages as $oFlihtVoyage)
+        {
+            switch ($sKey)
+            {
                 case "price":
-                    $sVal = intval( $oFlihtVoyage->price );
+                    $sVal = intval($oFlihtVoyage->price);
                     break;
             }
-            
-            if ( !isset( $aVariantsStacks[$sVal] ) ) {
+
+            if (!isset($aVariantsStacks[$sVal]))
+            {
                 $aVariantsStacks[$sVal] = new FlightVoyageStack();
-            
+
             }
-            $aVariantsStacks[$sVal]->addFlightVoyage( $oFlihtVoyage );
+            $aVariantsStacks[$sVal]->addFlightVoyage($oFlihtVoyage);
         }
-        uksort( $aVariantsStacks, 'FlightVoyageStack::compare_array' ); //sort array by key
-        reset( $aVariantsStacks );
-        $aEach = each( $aVariantsStacks );
+        uksort($aVariantsStacks, 'FlightVoyageStack::compare_array'); //sort array by key
+        reset($aVariantsStacks);
+        $aEach = each($aVariantsStacks);
         return $aVariantsStacks;
     }
 }
