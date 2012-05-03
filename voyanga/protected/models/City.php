4068639f9d78390710afb2599f90b879e8866e64
oleg <oleg@3dcf240c-a488-453e-83cb-a6019b294898>
2012-05-03 15:10:59 +0000

    Update of City object
    git-svn-id: svn://192.168.0.55/repo@17 3dcf240c-a488-453e-83cb-a6019b294898

--- voyanga/protected/models/City.php
+++ voyanga/protected/models/City.php
@@ -1,40 +1,130 @@
 <?php
 /**
- * City class
- * Contain information about city
- * @author oleg
+ * This is the model class for table "city".
  *
+ * The followings are the available columns in table 'city':
+ * @property integer $id
+ * @property integer $position
+ * @property integer $countryId
+ * @property string $code
+ * @property string $localRu
+ * @property string $localEn
+ *
+ * The followings are the available model relations:
+ * @property Airport[] $airports
+ * @property Country $country
+ * @property Route[] $routes
+ * @property Route[] $routes1
  */
-class City extends CActiveRecord {
-    public $id;
-    public $sCode;
-    private $_country;
-    public $country_id;
-    public $iPosition;
-    public $sLocalRu;
-    public $sLocalEn;
+class City extends CActiveRecord
+{
     
-    public static function model( $className = __CLASS__ ) {
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return City the static model class
+     */
+    public static function model($className = __CLASS__)
+    {
         return parent::model( $className );
     }
     
-    public function tableName() {
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
         return 'city';
     }
     
-    public function __get( $name ) {
-        if ( $name === 'country' ){
-            if ( !$this->_country ){
-                if ( $this->country_id ){
-                    $this->_country = Country::model()->findByPk( $this->country_id );
-                    return $this->_country;
-                }else{
-                    return NULL;
-                }
-            }else
-                return $this->_country;
-        }else{
-            return parent::__get( $name );
-        }
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+                array(
+                        'position, countryId', 
+                        'numerical', 
+                        'integerOnly' => true ), 
+                array(
+                        'code', 
+                        'length', 
+                        'max' => 5 ), 
+                array(
+                        'localRu, localEn', 
+                        'length', 
+                        'max' => 45 ), 
+                // The following rule is used by search().
+                // Please remove those attributes that should not be searched.
+                array(
+                        'id, position, countryId, code, localRu, localEn', 
+                        'safe', 
+                        'on' => 'search' ) );
+    }
+    
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+                'airports' => array(
+                        self::HAS_MANY, 
+                        'Airport', 
+                        'cityId' ), 
+                'country' => array(
+                        self::BELONGS_TO, 
+                        'Country', 
+                        'countryId' ), 
+                'departureRoutes' => array(
+                        self::HAS_MANY, 
+                        'Route', 
+                        'departureCityId' ), 
+                'arrivalRoutes' => array(
+                        self::HAS_MANY, 
+                        'Route', 
+                        'arrivalCityId' ) );
+    }
+    
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+                'id' => 'ID', 
+                'position' => 'Position', 
+                'countryId' => 'Country', 
+                'code' => 'Code', 
+                'localRu' => 'Local Ru', 
+                'localEn' => 'Local En' );
+    }
+    
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+        
+
+        $criteria = new CDbCriteria();
+        
+        $criteria->compare( 'id', $this->id );
+        $criteria->compare( 'position', $this->position );
+        $criteria->compare( 'countryId', $this->countryId );
+        $criteria->compare( 'code', $this->code, true );
+        $criteria->compare( 'localRu', $this->localRu, true );
+        $criteria->compare( 'localEn', $this->localEn, true );
+        
+        return new CActiveDataProvider( $this, array(
+                'criteria' => $criteria ) );
     }
 }
