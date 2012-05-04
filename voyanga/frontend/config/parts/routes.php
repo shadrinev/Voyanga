436e652f8aacde8d0e06f3a81c2bf2f3689494da
misha <misha@3dcf240c-a488-453e-83cb-a6019b294898>
2012-05-04 13:19:13 +0000

    cleanup config
    git-svn-id: svn://192.168.0.55/repo@56 3dcf240c-a488-453e-83cb-a6019b294898

--- voyanga/frontend/config/parts/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Routes for CUrlManager
- */
-return array(
-    '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-    '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-    '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-);
