74779dbfbc533d8313b23e61641c8249f08f3d20
oleg <oleg@3dcf240c-a488-453e-83cb-a6019b294898>
2012-05-12 13:54:55 +0000

    testing gds nemo
    git-svn-id: svn://192.168.0.55/repo@99 3dcf240c-a488-453e-83cb-a6019b294898

---
+++ voyanga/frontend/models/FlightSearchForm.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: oleg
+ * Date: 12.05.12
+ * Time: 11:27
+ * To change this template use File | Settings | File Templates.
+ */
+class FlightSearchForm extends CFormModel
+{
+    public $departureCityId;
+    public $departureCity;
+    public $departureAirportCode;
+    public $arrivalCityId;
+    public $arrivalCity;
+    public $arrivalAirportCode;
+    public $adultCount = 1;
+    public $childCount;
+    public $infantCount;
+    public $departureDate;
+
+    /**
+     * Declares the validation rules.
+     */
+    public function rules()
+    {
+        return array(
+            // first_name, last_name, number, birthday, document_type_id, gender_id are required
+            array(
+                'departureCityId, arrivalCityId, departureDate',
+                'required'),
+            // email has to be a valid birthday format
+            array(
+                'departureDate',
+                'date', 'format' => 'dd.MM.yyyy'),
+            array('adultCount, childCount, infantCount, departureCity, arrivalCity','safe')
+        );
+    }
+
+    /**
+     * Declares customized attribute labels.
+     * If not declared here, an attribute would have a label that is
+     * the same as its name with the first letter in upper case.
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'departureCityId' => 'Код города отправления',
+            'arrivalCityId' => 'Код города прибытия',
+            'adultCount' => 'Количество взрослых',
+            'childCount' => 'Количество детей',
+            'infantCount' => 'Количество младенцев'
+        );
+    }
+
+
+}
