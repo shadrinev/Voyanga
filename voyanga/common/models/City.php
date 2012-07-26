66805b7d2860edc63520f7c7db71b3fdab6253b2
oleg <oleg@3dcf240c-a488-453e-83cb-a6019b294898>
2012-07-26 13:16:36 +0000

    find city by name
    git-svn-id: svn://192.168.0.55/repo@352 3dcf240c-a488-453e-83cb-a6019b294898

--- voyanga/common/models/City.php
+++ voyanga/common/models/City.php
@@ -227,4 +227,113 @@ class City extends CActiveRecord
             }
         }
     }
+
+    public function guess($query){
+        $currentLimit = 1;
+        $items = null;//Yii::app()->cache->get('autocompleteCityForHotel'.$query);
+        if(!$items)
+        {
+            $items = array();
+            $cityIds = array();
+
+            if(strlen($query) == 3)
+            {
+                $criteria = new CDbCriteria();
+                $criteria->limit = $currentLimit;
+                $criteria->params[':code'] = $query;
+                $criteria->addCondition('t.code = :code');
+                $criteria->addCondition('t.hotelbookId > 0');
+                $criteria->with = 'country';
+                /** @var  City[] $cities  */
+                $cities = City::model()->findAll($criteria);
+
+                if($cities)
+                {
+                    foreach($cities as $city)
+                    {
+                        $items[] = array(
+                            'id'=>$city->primaryKey,
+                            'label'=>$this->parseTemplate('{localRu}, {country.localRu}, {code}',$city),
+                            'value'=>$this->parseTemplate('{localRu}',$city),
+                        );
+                        $cityIds[$city->id] = $city->id;
+                    }
+                }
+                $currentLimit -= count($items);
+            }
+            $criteria = new CDbCriteria();
+            $criteria->limit = $currentLimit;
+            $criteria->params[':localRu'] = $query.'%';
+            $criteria->params[':localEn'] = $query.'%';
+
+            $criteria->addCondition('t.localRu LIKE :localRu OR t.localEn LIKE :localEn');
+            $criteria->addCondition('t.hotelbookId > 0');
+            if($cityIds)
+            {
+                $criteria->addCondition('t.id NOT IN ('.join(',',$cityIds).')');
+            }
+            $criteria->with = 'country';
+            $criteria->order = 'country.position desc, t.position desc';
+            $cities = City::model()->findAll($criteria);
+
+            if($cities)
+            {
+                foreach($cities as $city)
+                {
+                    $items[] = array(
+                        'id'=>$city->primaryKey,
+                        'label'=>$this->parseTemplate('{localRu}, {country.localRu}, {code}',$city),
+                        'value'=>$this->parseTemplate('{localRu}',$city),
+                    );
+                    $cityIds[$city->id] = $city->id;
+                }
+            }
+            $currentLimit -= count($items);
+            if($currentLimit)
+            {
+                $criteria = new CDbCriteria();
+                $criteria->limit = $currentLimit;
+                if(UtilsHelper::countRussianCharacters($query) > 0)
+                {
+                    $nameRu = $query;
+                }else{
+
+                    $nameRu = UtilsHelper::cityNameToRus($query);
+                }
+                $metaphoneRu = UtilsHelper::ruMetaphone($nameRu);
+
+                if($metaphoneRu)
+                {
+                    $criteria->params[':metaphoneRu'] = $metaphoneRu;
+
+                    $criteria->addCondition('t.metaphoneRu = :metaphoneRu');
+                    $criteria->addCondition('t.hotelbookId > 0');
+                    if($cityIds)
+                    {
+                        $criteria->addCondition('t.id NOT IN ('.join(',',$cityIds).')');
+                    }
+                    $criteria->with = 'country';
+                    $criteria->order = 'country.position desc, t.position desc';
+                    $cities = City::model()->findAll($criteria);
+
+                    if($cities)
+                    {
+                        foreach($cities as $city)
+                        {
+                            $items[] = array(
+                                'id'=>$city->primaryKey,
+                                'label'=>$this->parseTemplate('{localRu}, {country.localRu}, {code}',$city),
+                                'value'=>$this->parseTemplate('{localRu}',$city),
+                            );
+                            $cityIds[$city->id] = $city->id;
+                        }
+                    }
+                    $currentLimit -= count($items);
+                }
+            }
+            Yii::app()->cache->set('autocompleteCityForHotel'.$query,$items,appParams('autocompleteCacheTime'));
+        }
+
+            return $items;
+    }
 }
