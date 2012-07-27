739290a018171e15ff9e20cb409e4646f10339be
Anatoly Kudinov <zz@rialabs.org>
2012-07-27 16:02:01 +0400

    [hotels rating] console command cleanup

--- console/commands/ImportRatingCommand.php
+++ console/commands/ImportRatingCommand.php
@@ -2,7 +2,7 @@
 /**
  * Command to insert/update hotels rating in database
  */
-class ImportRatingCommand extends CConsoleCommand
+class ImportRatingCommand extends FileProcessingCommand
 {
     public function getHelp()
     {
@@ -12,33 +12,9 @@ USAGE ImportRating path/to/csv/from/spider.csv
 EOD;
     }
 
-    public function run($args)
+    public function process($fh)
     {
-        if (count($args)==0)
-        {
-            echo $this->getHelp();
-            Yii::app()->end();
-        }
-
-        if (!is_readable($args[0]))
-        {
-            $this->logError("Given file is not readable by current user or does not exists.");
-            echo $this->getHelp();
-            Yii::app()->end();
-        }
-
-        Yii::import("common.modules.hotel.models.HotelRating");
-
-        $fh = fopen($args[0], 'r');
-
-        if($fh===FALSE)
-        {
-            // Should never happen in real life
-            $this->logError("File exists and readable, yet i cant open it for reading.");
-            echo $this->getHelp();
-            Yii::app()->end();
-        }
-        // skip und check header
+       // skip und check header
         $line = trim(fgets($fh));
         if($line!="rating,name,locality,url,country,name_alt,address,lat,lng,postal,address_ext")
         {
@@ -100,8 +76,5 @@ EOD;
         }
     }
 
-    public function logError($msg)
-    {
-        Yii::log($msg, 'error', 'console.importrating');
-    }
+
 }
