e5020f2dd731599b1c375e544723ee40c1f5d79e
Anatoly Kudinov <zz@rialabs.org>
2012-07-31 18:18:38 +0400

    [payments] more stubs

--- common/extensions/payments/PaymentsComponent.php
+++ common/extensions/payments/PaymentsComponent.php
@@ -10,16 +10,28 @@
 class PaymentsComponent extends CApplicationComponent
 {
     /**
-     * Uniteller merchant Id
+     * Uniteller shop id
      * @var int
      */
-    private $_merchantId;
+    private $_shopId;
+
+    /**
+     * Uniteller login. Required for some requests.
+     * @var bool
+     */
+    private $_login;
 
     /**
      * Salt for request signing
      * @var string
      */
     private $_password;
+
+    /**
+     * Test mode flag, tells us which api endpoints to use.
+     * @var bool
+     */
+    private $_testMode;
 
     public function createBill ($data)
     {
@@ -30,14 +42,22 @@ class PaymentsComponent extends CApplicationComponent
 
     }
 
-    public function getStatus()
+    public function getDataByOrderId($orderId)
     {
-
+        //! FIXME switch to post here
+        $data = Yii::app()->httpClient->get($this->statusUrl);
+        var_dump($data);
+        return Array();
     }
 
-    public function setMerchantId($value)
+    public function setShopId($value)
     {
-        $this->_merchantId = $value;
+        $this->_shopId = $value;
+    }
+
+    public function getShopId()
+    {
+        return $this->_shopId;
     }
 
     public function setPassword($value)
@@ -45,13 +65,37 @@ class PaymentsComponent extends CApplicationComponent
         $this->_password = $value;
     }
 
-    public function getMerchantId()
-    {
-        return $this->_merchantId;
-    }
-
-    public function getPassword()
+    /*    public function getPassword()
     {
         return $this->_password;
+        }*/
+
+    public function setTestMode($value)
+    {
+        $this->_testMode = $value;
+    }
+
+    public function setLogin($value)
+    {
+        $this->_login = $value;
+    }
+
+    public function getStatusUrl()
+    {
+        $result = $this->baseUrl . '/results/';
+        $result.= '?Shop_ID='. $this->shopId;
+        $result.= '&Login=' . $this->_login;
+        $result.= '&Password=' . $this->_password;
+        $result.= '&Format=1&Header=1';
+        return $result;
+    }
+
+    public function getBaseUrl()
+    {
+        if($this->_testMode)
+        {
+            return 'https://test.wpay.uniteller.ru';
+        }
+        return 'https://wpay.uniteller.ru';
     }
 }
