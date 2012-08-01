40d788ba38528a79cd7bbb7266480dd10c977691
maximov-ru <maximov@danechka.com>
2012-08-01 11:47:28 +0400

    booking for order

--- common/modules/tour/actions/constructor/MakeBookingAction.php
+++ common/modules/tour/actions/constructor/MakeBookingAction.php
@@ -10,15 +10,31 @@ class MakeBookingAction extends CAction
     public function run()
     {
         $trip = Yii::app()->order->getPositions(false);
+        $valid = true;
 
         foreach($trip as $cartElement)
         {
             if($cartElement instanceof FlightTripElement)
             {
-                $flightSearchParams = new FlightSearchParams();
-                $flightSearchParams->addRoute(array(''));
+                if(!$cartElement->flightVoyage)
+                {
+                    $valid = false;
+                }
+            }
+
+            if($cartElement instanceof HotelTripElement)
+            {
+                if(!$cartElement->hotel)
+                {
+                    $valid = false;
+                }
             }
         }
         VarDumper::dump(Yii::app()->shoppingCart);
+        if($valid)
+        {
+            Yii::app()->order->booking();
+        }
+        echo 123;die();
     }
 }
