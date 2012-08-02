65ab9021fbdd2563aeb1072cd53dedde52064373
maximov-ru <maximov@danechka.com>
2012-08-02 10:36:14 +0400

    booking for order

--- common/models/FlightTripElement.php
+++ common/models/FlightTripElement.php
@@ -102,30 +102,9 @@ class FlightTripElement extends TripElement
         {
             $count = $this->flightVoyage->adultPassengerInfo->count;
         }
-        $lastNames = array('Ivanov','Petrov','Kovalev','Mihailov','Romanov','Matveev','Borisov','Sviridov','Fedorov','Nikitin','Grigoriev','Vasilev');
-        $firstNames = array('Evgeniy','Kirill','Oleg','Mihail','Dmitriy','Roman','Denis','Artem','Danila','Viktor','Nikolay','Aleksey','Ruslan');
-
         for($i = 0;$i<$count;$i++)
         {
-            $adult1 = new FlightAdultPassportForm();
-            $adult1->genderId = FlightAdultPassportForm::GENDER_MALE;
-            $randKey = rand(0,(count($firstNames) - 1));
-            $randomFirstName = $firstNames[$randKey];
-            $randKey = rand(0,(count($lastNames) - 1));
-            $randomLastName = $lastNames[$randKey];
-            $randomBirthDay = new DateTime('1971-01-01');
-            $randKey = rand(0,365*20);
-            $interval = new DateInterval('P'.$randKey.'D');
-            $randomBirthDay->add($interval);
-            $adult1->firstName = $randomFirstName;
-            $adult1->lastName = $randomLastName;
-            $adult1->birthday = $randomBirthDay->format('d.m.Y');
-            $randKey = rand(1000,7000);
-            $adult1->series = (string)$randKey;
-            $randKey = rand(100000,999999);
-            $adult1->number = (string)$randKey;
-            $adult1->countryId = 174;
-            $fake->adultsPassports[] = $adult1;
+            $fake->adultsPassports[] = FlightAdultPassportForm::fillWithRandomData();
         }
         return $fake;
     }
