abeb437c8a4608835d71cd25a6ad906f41ad2919
Mikhail Kuklin <mihan007@ya.ru>
2012-08-17 11:36:53 +0400

    fix for hotel booking

--- common/components/order/OrderComponent.php
+++ common/components/order/OrderComponent.php
@@ -141,6 +141,7 @@ class OrderComponent extends CApplicationComponent
                                 //break;
                             }
                         }
+
                     }
                 }
             }
