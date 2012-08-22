38d697a3417f28fe1a73ace5c0922787f2675262
maximov-ru <maximov@danechka.com>
2012-08-22 19:29:26 +0400

    aircraft name adding

--- common/models/FlightPart.php
+++ common/models/FlightPart.php
@@ -66,6 +66,7 @@ class FlightPart
             'duration'=>$this->duration,
             'departureAirport'=>$this->departureAirport->localRu,
             'arrivalAirport'=>$this->arrivalAirport->localRu,
+            'aircraftName'=>$this->aircraftName,
 
         );
         return $ret;
