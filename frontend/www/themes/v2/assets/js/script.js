8fdd2e81afdc29c983f21e99a7510734d2c2b941
Anatoly Kudinov <zz@rialabs.org>
2012-09-19 19:08:15 +0400

    Possible fic

--- frontend/www/themes/v2/assets/js/script.js
+++ frontend/www/themes/v2/assets/js/script.js
@@ -1,121 +1,112 @@
 function sliderPhoto(that) {
-	var var_this = $(that).eq(0);
-	var var_len = var_this.find('ul > li').length;
-		var_this.find('ul li').eq(0).addClass('active');
-	var var_widthAll = var_this.width();
-	var var_widthUL = 0;
-	for (a = 0; a < var_len; a++) {
-		var_widthUL += var_this.find('ul li').eq(a).find('img').width() + 1;
+    var var_this = $(that).eq(0);
+    var var_len = var_this.find('ul > li').length;
+    var_this.find('ul li').eq(0).addClass('active');
+    var var_widthAll = var_this.width();
+    var var_widthUL = 0;
+    for (a = 0; a < var_len; a++) {
+	var_widthUL += var_this.find('ul li').eq(a).find('img').width() + 1;
         console.log("UL LENGTH", var_widthUL, var_this.find('ul li').eq(a).find('img').width() + 1);
-	}
-	var_this.find('ul').css('width', var_widthUL+'px');
-	$('.left-navi').hide();
+    }
+    var_this.find('ul').css('width', var_widthUL+'px');
+    $('.left-navi').hide();
 
-	var var_widthULminus = var_widthUL - var_widthAll;
-	var one_short = false;
+    var var_widthULminus = var_widthUL - var_widthAll;
+    var one_short = false;
 
     $('.right-navi').unbind('click');
-	$('.right-navi').click(function() {
+    $('.right-navi').click(function() {
 	if (! one_short) {
-		one_short = true;
-		var_this.find('.left-navi').show();
-		var var_widthOne = (var_this.find('ul .active').width() + 1);
-		var var_margin = var_this.find('ul').css('margin-left');
-			var_margin = Math.abs(parseInt(var_margin.slice(0,-2)));
-		var var_index = var_this.find('ul .active').index();
-		var all = var_margin+var_widthOne;
-		if (all <= var_widthULminus) {
-			var_this.find('ul').animate({'margin-left' : '-='+var_widthOne+'px'}, 200, function() {
-						one_short = false;
-					});
-			var_this.find('ul .active').removeClass('active').next().addClass('active');
-		}
-		else {
-			$(this).hide();
-			var_widthOne = (var_this.find('ul li').eq(var_len-1).width() + 1);
-			console.log(var_widthOne +' '+all+' '+var_widthUL+' '+var_widthULminus+' '+var_widthAll);
-			var var_widthEnd = (var_widthUL - var_widthAll) - 3;
-			var_this.find('ul').animate({'margin-left' : '-'+var_widthEnd+'px'}, 200, function() {
-						one_short = false;
-					});
-			var_this.find('ul li').removeClass('active');
-			var_this.find('ul li').eq(var_len-1).addClass('active');
-		}
+	    one_short = true;
+	    var_this.find('.left-navi').show();
+	    var var_widthOne = (var_this.find('ul .active').width() + 1);
+	    var var_margin = var_this.find('ul').css('margin-left');
+	    var_margin = Math.abs(parseInt(var_margin.slice(0,-2)));
+	    var var_index = var_this.find('ul .active').index();
+	    var all = var_margin+var_widthOne;
+	    if (all <= var_widthULminus) {
+		var_this.find('ul').animate({'margin-left' : '-='+var_widthOne+'px'}, 200, function() {
+		    one_short = false;
+		});
+		var_this.find('ul .active').removeClass('active').next().addClass('active');
+	    }
+	    else {
+		$(this).hide();
+		var_widthOne = (var_this.find('ul li').eq(var_len-1).width() + 1);
+		console.log(var_widthOne +' '+all+' '+var_widthUL+' '+var_widthULminus+' '+var_widthAll);
+		var var_widthEnd = (var_widthUL - var_widthAll) - 3;
+		var_this.find('ul').animate({'margin-left' : '-'+var_widthEnd+'px'}, 200, function() {
+		    one_short = false;
+		});
+		var_this.find('ul li').removeClass('active');
+		var_this.find('ul li').eq(var_len-1).addClass('active');
+	    }
 	}
-	});
+    });
     $('.left-navi').unbind('click');
-	$('.left-navi').click(function() {
+    $('.left-navi').click(function() {
 	if (! one_short) {
-		one_short = true;
-		var_this.find('.right-navi').show();
-		var var_widthOne = (var_this.find('ul .active').width() + 1);
-		var var_margin = var_this.find('ul').css('margin-left');
-		var_margin = Math.abs(parseInt(var_margin.slice(0,-2)));
-		if ((var_margin-var_widthOne) > 0) {
-			var_this.find('ul').animate({'margin-left' : '+='+var_widthOne+'px'}, 200, function() {
-						one_short = false;
-					});
-			var_this.find('ul .active').removeClass('active').prev().addClass('active');
-		}
-		else {
-			var_this.find('ul').animate({'margin-left' : '0px'}, 200, function() {
-						one_short = false;
-					});
-			var_this.find('ul li').removeClass('active');
-			var_this.find('ul li').eq(0).addClass('active');
-			$(this).hide();
-		}
+	    one_short = true;
+	    var_this.find('.right-navi').show();
+	    var var_widthOne = (var_this.find('ul .active').width() + 1);
+	    var var_margin = var_this.find('ul').css('margin-left');
+	    var_margin = Math.abs(parseInt(var_margin.slice(0,-2)));
+	    if ((var_margin-var_widthOne) > 0) {
+		var_this.find('ul').animate({'margin-left' : '+='+var_widthOne+'px'}, 200, function() {
+		    one_short = false;
+		});
+		var_this.find('ul .active').removeClass('active').prev().addClass('active');
+	    }
+	    else {
+		var_this.find('ul').animate({'margin-left' : '0px'}, 200, function() {
+		    one_short = false;
+		});
+		var_this.find('ul li').removeClass('active');
+		var_this.find('ul li').eq(0).addClass('active');
+		$(this).hide();
+	    }
 	}
-	});
+    });
 }
 function checkUlList() {
-	$('.details').each(function() {
-		var var_this = $(this).find('ul li');
-		var var_length = var_this.length;
-			for (i = 0; i < var_length; i++) {
-				if (i == 0 || i == 1) {
-					var_this.eq(i).addClass('not-show');
-				}
-				else {
-					var_this.eq(i).hide();
-				}
-		}
-	});
-	$('.tab-ul a').click(function() {
-		var var_thisLink = $(this);
-		var var_this = $(this).parent().parent();
-		if (! $(this).hasClass('active')) {
-			var_thisLink.text('Свернуть все рузультаты');
-			var_thisLink.addClass('active');
-			var_this.find('ul li[class != "not-show"]').slideDown();
-		}
-		else {
-			var_this.find('ul li[class != "not-show"]').slideUp(300, function() {
-				var_thisLink.removeClass('active');
-				var_thisLink.text('Посмотреть все результаты');
-			});
-		}
-	});
-
+    $('.details').each(function() {
+	var var_this = $(this).find('ul li');
+	var var_length = var_this.length;
+	for (i = 0; i < var_length; i++) {
+	    if (i == 0 || i == 1) {
+		var_this.eq(i).addClass('not-show');
+	    }
+	    else {
+		var_this.eq(i).hide();
+	    }
+	}
+    });
+    $('.tab-ul a').click(function() {
+	var var_thisLink = $(this);
+	var var_this = $(this).parent().parent();
+	if (! $(this).hasClass('active')) {
+	    var_thisLink.text('Свернуть все рузультаты');
+	    var_thisLink.addClass('active');
+	    var_this.find('ul li[class != "not-show"]').slideDown();
+	}
+	else {
+	    var_this.find('ul li[class != "not-show"]').slideUp(300, function() {
+		var_thisLink.removeClass('active');
+		var_thisLink.text('Посмотреть все результаты');
+	    });
+	}
+    });
+    
 }
 function hideRecomendedBlockTicket() {
-	e.preventDefault();
-	$('.recomended-content').slideUp();
-	$('.minimize-rcomended .btn-minimizeRecomended').animate({top : '0px'}, 500);
-	$(this).fadeOut();
+    $('.recomended-content').slideUp();
+    $('.minimize-rcomended .btn-minimizeRecomended').animate({top : '0px'}, 500);
+    $(this).fadeOut();
 }
-$(window).load(checkUlList);
+//$(window).load(checkUlList);
 $(function() {
 
 	$('.minimize-rcomended .btn-minimizeRecomended').click(function() {
-		$('.recomended-content').slideDown();
-		$(this).animate({top : '-19px'}, 500);
-		$(window).load(inTheTwoLines);
-
-		otherTimeSlide();
-		widthHowLong();
-		setTimeout(smallTicketHeight, 100);
-		$('.order-hide').fadeIn();
 	});
 	$('.order-show').click(function() {
 		$('.recomended-content').slideDown();
