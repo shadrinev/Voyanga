e39e0c28b8d711a82610f17c74af9a46832d4e59
Mikhail Kuklin <mihan007@ya.ru>
2012-09-20 13:48:02 +0400

    refundable and freeWeight, description

--- api/modules/v1/modules/flight/controllers/SearchController.php
+++ api/modules/v1/modules/flight/controllers/SearchController.php
@@ -130,7 +130,15 @@ class SearchController extends ApiController
         {
             $newFlight = $flight;
             if ($injectSearchParams)
+            {
                 $newFlight['serviceClass'] = $injectSearchParams['serviceClass'];
+                $newFlight['freeWeight'] = ($newFlight['serviceClass'] == 'E') ? $flight['economFreeWeight'] : $flight['businessFreeWeight'];
+                $newFlight['description'] = ($newFlight['serviceClass'] == 'E') ? $flight['economDescription'] : $flight['businessDescription'];
+                unset($newFlight['economFreeWeight']);
+                unset($newFlight['businessFreeWeight']);
+                unset($newFlight['economDescription']);
+                unset($newFlight['businessDescription']);
+            }
             $newFlights[] = $newFlight;
         }
         return $newFlights;
