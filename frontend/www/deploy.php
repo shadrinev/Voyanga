e1f16a85147bd40469b0172e97f2de7fa7a42e1b
Mikhail Kuklin <mihan007@ya.ru>
2012-09-25 12:42:23 +0400

    deploy done

--- frontend/www/deploy.php
+++ frontend/www/deploy.php
@@ -1,18 +1,29 @@
 <?php
-$result = exec("cd ~ && ls");
-var_dump($result);
-
-$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
-
-if (in_array($_SERVER['REMOTE_ADDR'], $github_ips))
+function _exec($cmd)
 {
-    $dir = '/home/voyanga/app/';
-    exec("cd $dir && git pull && $dir/yiic migrate --interactive=0");
-    echo 'Done.';
+    $out = "$ " . $cmd . "\n";
+    $out .= shell_exec($cmd . ' 2>&1');
+    _log($out);
 }
+
+function _log($str)
+{
+    $out = "\n" . date('r') . " " . $str;
+    echo $out;
+    error_log($out, 3, '.webhook.log');
+}
+
+//$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
+
+/*if (in_array($_SERVER['REMOTE_ADDR'], $github_ips))
+{*/
+    $dir = '/home/voyanga/app/';
+    $result = _exec("cd $dir && git pull && $dir/yiic migrate --interactive=0");
+    echo 'Done.';
+/*}
 else
 {
     header('HTTP/1.1 404 Not Found');
     echo '404 Not Found.';
     exit;
-}
+}*/
