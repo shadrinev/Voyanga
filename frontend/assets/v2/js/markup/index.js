05c881ec9096c6d211f0bf9b3bd24278e788d929
severenit <severenit@gmail.com>
2012-10-23 17:05:17 +0400

    event

--- frontend/assets/v2/js/markup/index.js
+++ frontend/assets/v2/js/markup/index.js
@@ -6,19 +6,20 @@ function slideToursSlide() {
 	var var_allWidth = $('.slideTours .center').width();
 	var var_widthTours;
 	if (var_allWidth >= 1390) {
-		var_lengthTours = 6;
+		var_lengthTours = 8;
 		var_widthTours = Math.floor(((var_allWidth / var_lengthTours) - 2));
 	}
 	else if (var_allWidth < 1390 && var_allWidth > 1290) {
-		var_lengthTours = 5;
+		var_lengthTours = 7;
 		var_widthTours = Math.floor(((var_allWidth / var_lengthTours) - 2));
 	}
 	else if (var_allWidth < 1290 && var_allWidth > 1000) {
-		var_lengthTours = 4;
+		var_lengthTours = 6;
 		var_widthTours = Math.floor(((var_allWidth / var_lengthTours) - 2));
 	}
 	else if (var_allWidth <= 1000) {
-		var_widthTours = 248;
+		var_lengthTours = 5;
+		var_widthTours = Math.floor(((var_allWidth / var_lengthTours) - 2));
 	}
 	var_slideToursBody.find('.toursTicketsMain').css('width',var_widthTours+'px');
 }
