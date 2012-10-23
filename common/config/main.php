e942c9be19edd1f4069ed486b2abb3cce2151346
Mikhail Kuklin <mihan007@ya.ru>
2012-10-23 17:28:42 +0400

    common prefix

--- common/config/main.php
+++ common/config/main.php
@@ -47,6 +47,7 @@ return array(
 
         'pCache' => array(
             'class' => 'site.common.extensions.MongoDbCache.MongoDbCache',
+            'prefix' => 'voyanga-',
             'mongoConnectionId' => 'mongodb',
             'hashKey' => false,
             'serializer' => array('igbinary_serialize', 'igbinary_unserialize')
