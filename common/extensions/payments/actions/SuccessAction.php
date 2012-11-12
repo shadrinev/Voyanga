8342337ad7c5fd053fc72b863f9481ce25c4427a
Anatoly Kudinov <zz@rialabs.org>
2012-11-12 19:52:45 +0400

    Rebills testing

--- common/extensions/payments/actions/SuccessAction.php
+++ common/extensions/payments/actions/SuccessAction.php
@@ -27,14 +27,29 @@ class SuccessAction extends CAction
         if($bill->transactionId)
             throw new Exception("Bill already have transaction id");
         $bill->transactionId = $params['TransactionID'];
-        $bill->booker->status('paid');
+        $channel->booker->status('paid');
         $bill->save();
         echo 'Ok';
         // init order
-#        $order = Yii::app()->order;
-#        $order->initByOrderBookingId($orderId);
-#        $bookers = $order->getBookers();
-#        for($bookers as $booker) {
-#        }
+        $order = Yii::app()->order;
+        $order->initByOrderBookingId($orderId);
+        $payments = Yii::app()->payments;
+        $bookers = $order->getBookers();
+        foreach($bookers as $booker)
+        {
+            if($booker->getStatus()=='paid')
+                continue;
+            $order->isWaitingForPaymentState($booker->getStatus());
+            $bill = $payments->getBillForBooker($booker);
+            $channel = $bill->getChannel();
+            if($channel->rebill($_REQUEST['RebillAnchor']))
+            {
+                $booker->status('paid');
+            }
+            else
+            {
+                throw new Exception("REFUND ALL THE THINGS");
+            }
+        }
     }
 }
