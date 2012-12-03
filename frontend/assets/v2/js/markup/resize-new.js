c42c29bf70486c7a985e5e5a332efd23b5771cb7
severenit <severenit@gmail.com>
2012-12-03 16:02:26 +0400

    Gradient

--- frontend/assets/v2/js/markup/resize-new.js
+++ frontend/assets/v2/js/markup/resize-new.js
@@ -571,6 +571,7 @@ function ResizeAvia() {
     slideToursSlide();
     smallIMGresizeIndex();
     mapAllPageView();
+    gradientResize();
 }
 
 function ResizeFun() {
@@ -1319,3 +1320,15 @@ function scrollFAQ() {
 $(window).load(resizeFAQ);
 $(window).resize(resizeFAQ);
 $(window).scroll(scrollFAQ);
+
+function gradientResize() {
+    if ($('.wrapper .main-block').find('#content').length > 0 && $('.wrapper .main-block').find('#content').is(':visible')) {
+        var _content = $('.wrapper .main-block').find('#content');
+        var offset = _content.offset();
+        var _leftContent = $(window).width() - offset.left;
+        var _rightContent = offset.left + _content.width();
+        $('.gShR').css('left', _rightContent+'px');
+        $('.gShL').css('right', _leftContent+'px');
+        console.log(_leftContent+' '+_rightContent);
+    }
+}
--- frontend/www/themes/v2/views/layouts/main.php
+++ frontend/www/themes/v2/views/layouts/main.php
@@ -84,7 +84,7 @@ Yii::app()->clientScript->registerScriptFile('/js/runApp.js');
             <div class="rightPageBtn" data-bind="swapPanel: {to: fakoPanel().nextPanel}"></div>
         </div>
         <!-- CALENDAR -->
-        <div class="calenderWindow z-indexTop" data-bind="template: {name: 'calendar-template-hotel', afterRender: reRenderCalendar}" style="top: -302px; height: 0;"></div>
+        <div class="calenderWindow z-indexTop" data-bind="template: {name: 'calendar-template-hotel', afterRender: reRenderCalendar}" style="top: -302px; display: none;"></div>
         <!-- END CALENDAR -->
     </div>
     <!-- /ko -->
@@ -130,8 +130,12 @@ Yii::app()->clientScript->registerScriptFile('/js/runApp.js');
         </div>
     </div>
     <!-- END FOOTER-->
+
 </div>
-</div>
+<div class="gShL"></div>
+<div class="gShR"></div>
+
+
 <!-- END WRAPPER -->
 <!-- MAPS -->
 <!-- FIXME -->
