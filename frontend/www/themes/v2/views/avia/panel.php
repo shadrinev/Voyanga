f11c78593c5853be95ae8991e32f73db3079465a
severenit <severenit@gmail.com>
2012-12-05 14:49:50 +0400

    fixed

--- frontend/www/themes/v2/views/avia/panel.php
+++ frontend/www/themes/v2/views/avia/panel.php
@@ -4,6 +4,11 @@
       <td class="contTD">
         <div class="data">
           <div class="from" data-bind="css: {active: fromChosen}">
+              <div class="bgInput">
+                  <div class="left"></div>
+                  <div class="center"></div>
+                  <div class="right"></div>
+              </div>
             <input class="input-path departureCity" type="text"  tabindex="-1">
             <input class="second-path departureCity" type="text" placeholder="Откуда" data-bind="autocomplete: {source:'city/airport_req/1', iata: departureCity, readable: departureCityReadable, readableAcc: departureCityReadableAcc, readableGen: departureCityReadableGen}">
               <div class="date" data-bind="click: showCalendar">
@@ -21,6 +26,11 @@
             <input id="there-back" type="checkbox" data-bind="checked: rt()">
           </div>
           <div class="to" data-bind="css: {active: rtFromChosen}">
+              <div class="bgInput">
+                  <div class="left"></div>
+                  <div class="center"></div>
+                  <div class="right"></div>
+              </div>
             <input class="input-path arrivalCity" type="text"  tabindex="-1">
             <input class="second-path arrivalCity" placeholder="Куда" data-bind="autocomplete: {source:'city/airport_req/1', iata: arrivalCity, readable: arrivalCityReadable, readableAcc: arrivalCityReadableAcc, readableGen: arrivalCityReadableGen}">
             <div class="date" data-bind="click: showCalendar">
--- frontend/www/themes/v2/views/hotels/panel.php
+++ frontend/www/themes/v2/views/hotels/panel.php
@@ -3,6 +3,11 @@
     <tr>
       <td class="tdCity">
         <div class="data" data-bind="css: {active: haveDates()}">
+            <div class="bgInput">
+                <div class="left"></div>
+                <div class="center"></div>
+                <div class="right"></div>
+            </div>
             <input class="input-path" tabindex="-1" type="text" data-bind="autocomplete: {source:'city/hotel_req/1', iata: city, readable: cityReadable, readableAcc: cityReadableAcc, readableGen: cityReadableGen}">
             <input class="second-path" type="text" placeholder="Куда едем?" data-bind="autocomplete: {source:'city/hotel_req/1', iata: city, readable: cityReadable, readableAcc: cityReadableAcc, readableGen: cityReadableGen}">
             <div class="date" data-bind="click: showCalendar, html:checkInHtml()">
