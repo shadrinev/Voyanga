d4201f235e57b67138d4208fbaf994404cf3c864
mihan007 <mihan007@ya.ru>
2012-12-15 23:31:22 +0400

    fot -> direction

--- frontend/www/themes/v2/views/buy/flightPart.php
+++ frontend/www/themes/v2/views/buy/flightPart.php
@@ -34,13 +34,10 @@
         </table>
         <div class="tdPrice">
             <div class="verticalAlign">
-                <div class="people">2 человека</div>
-                <div class="price"><span >12 500</span><span class="rur">o</span></div>
-                <div class="priceSale">
-                    <!-- <div class="lastPrice">13 000 <span class="rur">o</span></div> <span class="icoTours"></span> -->
-                </div>
+                <div class="people" data-bind="text: $data[0].totalPeople">2 человека</div>
+                <div class="price"><span data-bind="text:$data[0].price">12 500</span><span class="rur">o</span></div>
             </div>
-            <div class="moreDetails"><a href="#" >Подробнее</a></div>
+            <div class="moreDetails"><a href="#" data-bind="click: $data[0].showDetails">Подробнее</a></div>
         </div>
     </div>
 </script>
