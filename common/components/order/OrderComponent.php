603d0eb39edab5e0824259451c43ec04e2bb4f3c
maximov-ru <maximov@danechka.com>
2012-12-20 17:07:27 +0400

    cancel charges for hotel

--- common/components/order/OrderComponent.php
+++ common/components/order/OrderComponent.php
@@ -404,13 +404,14 @@ class OrderComponent extends CApplicationComponent
     {
         $pdf = Yii::app()->pdfGenerator;
         $pdfFileNames = array();
+        $orderBooking = $this->getOrderBooking();
         foreach ($this->itemsOnePerGroup as $item)
         {
             if ($item instanceof HotelTripElement)
             {
                 if ($item->hotelBookerId)
                 {
-                    if ($pdfFileName =  $pdf->forHotelItem($item))
+                    if ($pdfFileName =  $pdf->forHotelItem($item,$orderBooking->readableId))
                        $pdfFileNames[] = array('type'=>'hotel','filename'=>$pdfFileName);
                 }
             }
@@ -418,14 +419,14 @@ class OrderComponent extends CApplicationComponent
             {
                 if ($item->flightBookerId)
                 {
-                    if ($pdfFileName =  $pdf->forFlightItem($item))
+                    if ($pdfFileName =  $pdf->forFlightItem($item,$orderBooking->readableId))
                         $pdfFileNames[] = array('type'=>'avia','filename'=>$pdfFileName);
                 }
             }
         }
-        $orderBooking = $this->getOrderBooking();
+
         EmailManager::sendEmailOrderInfo(array(
-            'orderBookingId'=>$pdf->orderBookingId,
+            'orderBookingId'=>$orderBooking->readableId,
             'email'=>$orderBooking->email
         ),$pdfFileNames);
         foreach($pdfFileNames as $pdfInfo)
