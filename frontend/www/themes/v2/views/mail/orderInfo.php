58aeb1f9063ebe6fa011489d891af548cb0fdfdd
shadrinev <shadrin@voyanga.com>
2012-12-26 15:18:38 +0400

    Update frontend/www/themes/v2/views/mail/orderInfo.php
    new text for email

--- frontend/www/themes/v2/views/mail/orderInfo.php
+++ frontend/www/themes/v2/views/mail/orderInfo.php
@@ -1,5 +1,6 @@
 <!--- <?php $link = Yii::app()->createAbsoluteUrl('/user/orders'); ?> --->
 <?php $link = 'http://www.test.voyanga.com/user/orders' ?>
-<p>Ваш заказ готов!
-<p>Все ваши заказы доступны по адресу: <?php echo CHtml::link($link, $link); ?>. </p>
+<p>Дорогой друг!</p>
+<p>Ваш заказ готов. Все заказы доступны по адресу: <?php echo CHtml::link($link, $link); ?>. </p>
+<p>К письму прикреплены выписанные электронные билеты.</p>
 <p>Спасибо!</p>
