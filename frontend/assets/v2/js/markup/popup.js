c57f7abb555fe3f08e83d993d5c9d00c021b9a6b
severenit <severenit@gmail.com>
2013-01-21 14:25:54 +0400

    М/Ж

--- frontend/assets/v2/js/markup/popup.js
+++ frontend/assets/v2/js/markup/popup.js
@@ -3,45 +3,6 @@ function SizeBox(id) {
 	if ($('#' + id).length > 0 && $('#' + id).is(':visible')) {
 		$('body').css('overflow','hidden');
 	}
-	
-	
-	/*
-$(document).on('keydown', function(e){
-		var modalKeys = [40,38,37,39,36,35,33,34];
-		if ($.inArray(e.which, modalKeys) >= 0) {
-			e.preventDefault();
-			$('.' + localOptions.overlayClass).trigger(e);
-		}
-	});
-
-	
-	$(window).on('keydown', function(e){
-		if ($(e.target) != popup) {
-			var modalKeys = [40,38,37,39,36,35,33,34];
-			if ($.inArray(e.which, modalKeys) >= 0) {
-				e.preventDefault();
-				popup.trigger(e);
-				console.log(e);
-			}
-		}
-e.stopPropagation();
-		return false;
-
-	});
-	
-
-	var boxContent = popup.find('#boxContent');
-	var boxHeight = boxContent.innerHeight();
-	var boxWidth = boxContent.innerWidth();
-	var boxPopUpWidth = popup.innerWidth();
-	var boxPopUpHeight = popup.innerHeight();
-	var heightWindowAll = $('.popupBody').innerHeight() + $('.wrapper').scrollTop();
-	//popup.css('height', (heightWindowAll)+'px');
-	popup.find('#boxMiddleLeft').css('height', boxHeight+'px');
-	popup.find('#boxMiddleRight').css('height', boxHeight+'px');
-	popup.find('#boxTopCenter').css('width', boxWidth+'px');
-	popup.find('#boxBottomCenter').css('width', boxWidth+'px');
-*/
 }
 
 function ResizeBox(id) {
@@ -73,22 +34,6 @@ function ResizeBox(id) {
 		layer.css('width', (winWidth-16)+'px');
 		pvCont.css('padding-top', paddingTopPopUp+'px');
 	});
-	/*
-
-	var boxContent = popup.find('.popupBody');
-	var boxPopUpWidth = boxContent.innerWidth();
-	var boxPopUpHeight = boxContent.innerHeight();
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	var scrollTopMine = $('.wrapper').scrollTop();
-	var paddingLeft = (windowWidth - boxPopUpWidth) / 2;
-	var paddingTop = (windowHeight - boxPopUpHeight) / 2;
-	if (paddingTop < 0) {
-		paddingTop = 0;
-	}
-	boxContent.css('left', paddingLeft+'px').css('top', (paddingTop)+'px');
-	$('.wrapper').die('scroll',function() { });
-*/
 }
 function btnClosePopUp() {
 	$('body').css('overflow','auto');
