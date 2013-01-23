06612bafdbb7b88c401bd323aa5082273256e7d8
severenit <severenit@gmail.com>
2013-01-23 14:51:01 +0400

    helpers.js

--- frontend/assets/v2/js/markup/resize-new.js
+++ frontend/assets/v2/js/markup/resize-new.js
@@ -32,27 +32,7 @@ var var_paddingLeftTelefonMIN = 220;
 
 var var_widthMainBlockMAX = 695;
 var var_widthMainBlockMIN = 530;
-
-var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
+var var_iphone = 0;
 
 window.hotelsScrollCallback = function(){}
 
