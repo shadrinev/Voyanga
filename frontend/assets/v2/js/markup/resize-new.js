a57cd0d65d919f931fda05c0027cc870093bd43b
severenit <severenit@gmail.com>
2013-01-25 16:43:18 +0400

    css

--- frontend/assets/v2/js/markup/resize-new.js
+++ frontend/assets/v2/js/markup/resize-new.js
@@ -751,122 +751,127 @@ function jsPaneScrollHeight() {
 }
 
 function scrollValue(what, event) {
-	var filterContent = $('.filter-content.'+ what);
-    var isScrollPane;
-    if(event.target == document)
-	isScrollPane = false;
-    else
-	isScrollPane = $(event.target).is('#scroll-pane');
-    if (filterContent.length > 0 && filterContent.is(':visible') && !isScrollPane) {		
-		var innerFilter = filterContent.find('.innerFilter');
-		var var_marginTopSubHead = $('.sub-head').css('margin-top');
-		var var_scrollValueTop = $(window).scrollTop();
-		var var_heightWindow = $(window).height();
-		var var_heightContent = $('#content').height();
-		
-		if (what == 'avia') {
-			var var_topFilterContent = 73;
-			if ($('.sub-head').css('margin-top') != '-67px') {
-				var diffrentScrollTop = 173;
-			}
-			else {
-				var diffrentScrollTop = 110;
-			}
-		}
-		else {
-			var var_topFilterContent = 23;
-			if ($('.sub-head').css('margin-top') != '-67px') {
-				var diffrentScrollTop = 125;
-			}
-			else {
-				var diffrentScrollTop = 61 ;
-			}
-		}	
-		if (_GoOnScroll) {
-			var needDel = false;
-			if (var_scrollValueTop == 0) {
-				//is del
-				needDel = true;
-				filterContent.css('position','relative').css('top','auto').css('bottom','auto');
-			}
-			else if (var_scrollValueTop > 0 && var_scrollValueTop < diffrentScrollTop ) {
-				needDel = true;
-				filterContent.css('position','relative').css('top','auto').css('bottom','auto');
-			}
-			else if (var_scrollValueTop > diffrentScrollTop) {		
-				if (var_scrollValueTop > (($('.wrapper').height() - var_heightWindow) - 30)) {
-					var var_minHeightBottom;
-					filterContent.css('position','fixed').css('top','-'+var_topFilterContent+'px').css('bottom','auto');
-					if ((var_scrollValueTop - (($('.wrapper').height() - var_heightWindow) - 30)) < 30) {
-						var_minHeightBottom = (var_scrollValueTop - (($('.wrapper').height() - var_heightWindow) - 30));
-					}
-					else {
-						var_minHeightBottom = 30;
-					}
-					innerFilter.css('height', (var_heightWindow - var_minHeightBottom) +'px');
-					if(!$('#scroll-pane').data('jsp')){
-						$('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
-						
-					}
-					$('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});					
-					if(!_jScrollingBootom && var_scrollValueTop == ($('.wrapper').height() - $('body').height())){
-						_jScrollingBootom = true;
-	                    window.setTimeout(function(){
-						        $('#scroll-pane').data('jsp').scrollToBottom();
-	                        },
-	                        50
-	                    );
-						
-						window.setTimeout(
-							function(){
-								_jScrollingBootom = false;
-								_jScrollNonBottomInitted = false;
-								//scrollValue(what, event)
-							}
-							, 500
-						);
-						
-					}
-					//
-				}
-				else {
-					filterContent.css('position','fixed').css('top','-'+var_topFilterContent+'px').css('bottom','auto');
-					innerFilter.css('height', var_heightWindow +'px');
-					if(!$('#scroll-pane').data('jsp')){
-						$('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
-					}
-					if(!_jScrollNonBottomInitted){
-						_jScrollNonBottomInitted = true;
-						$('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
-						//$('#scroll-pane').data('jsp').scrollToBottom();
-					}
-				}
-				
-			}
-			if(needDel){
-				if($('#scroll-pane').data('jsp')){
-					$('#scroll-pane').data('jsp').destroy();
-				}
-			}
-		}
-		else {
-			var _issetMaps = $('#all-hotels-map').length > 0 && $('#all-hotels-map').is(':visible');
-			var _issetLeftBlock = $('.left-block').length > 0 && $('.left-block').is(':visible');
-			if (_issetMaps && ! _issetLeftBlock) {
-				
-			}
-			else {
-				if($('#scroll-pane').data('jsp')){
-					$('#scroll-pane').data('jsp').destroy();
-				}
+    if (! DetectIphoneOrIpod() ) {
+        var filterContent = $('.filter-content.'+ what);
+        var isScrollPane;
+        if(event.target == document)
+        isScrollPane = false;
+        else
+        isScrollPane = $(event.target).is('#scroll-pane');
+        if (filterContent.length > 0 && filterContent.is(':visible') && !isScrollPane) {
+            var innerFilter = filterContent.find('.innerFilter');
+            var var_marginTopSubHead = $('.sub-head').css('margin-top');
+            var var_scrollValueTop = $(window).scrollTop();
+            var var_heightWindow = $(window).height();
+            var var_heightContent = $('#content').height();
 
-			}
-			return false;
-		}
-	}
-	else {
-		return false;
-	}
+            if (what == 'avia') {
+                var var_topFilterContent = 73;
+                if ($('.sub-head').css('margin-top') != '-67px') {
+                    var diffrentScrollTop = 173;
+                }
+                else {
+                    var diffrentScrollTop = 110;
+                }
+            }
+            else {
+                var var_topFilterContent = 23;
+                if ($('.sub-head').css('margin-top') != '-67px') {
+                    var diffrentScrollTop = 125;
+                }
+                else {
+                    var diffrentScrollTop = 61 ;
+                }
+            }
+            if (_GoOnScroll) {
+                var needDel = false;
+                if (var_scrollValueTop == 0) {
+                    //is del
+                    needDel = true;
+                    filterContent.css('position','relative').css('top','auto').css('bottom','auto');
+                }
+                else if (var_scrollValueTop > 0 && var_scrollValueTop < diffrentScrollTop ) {
+                    needDel = true;
+                    filterContent.css('position','relative').css('top','auto').css('bottom','auto');
+                }
+                else if (var_scrollValueTop > diffrentScrollTop) {
+                    if (var_scrollValueTop > (($('.wrapper').height() - var_heightWindow) - 30)) {
+                        var var_minHeightBottom;
+                        filterContent.css('position','fixed').css('top','-'+var_topFilterContent+'px').css('bottom','auto');
+                        if ((var_scrollValueTop - (($('.wrapper').height() - var_heightWindow) - 30)) < 30) {
+                            var_minHeightBottom = (var_scrollValueTop - (($('.wrapper').height() - var_heightWindow) - 30));
+                        }
+                        else {
+                            var_minHeightBottom = 30;
+                        }
+                        innerFilter.css('height', (var_heightWindow - var_minHeightBottom) +'px');
+                        if(!$('#scroll-pane').data('jsp')){
+                            $('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
+
+                        }
+                        $('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
+                        if(!_jScrollingBootom && var_scrollValueTop == ($('.wrapper').height() - $('body').height())){
+                            _jScrollingBootom = true;
+                            window.setTimeout(function(){
+                                    $('#scroll-pane').data('jsp').scrollToBottom();
+                                },
+                                50
+                            );
+
+                            window.setTimeout(
+                                function(){
+                                    _jScrollingBootom = false;
+                                    _jScrollNonBottomInitted = false;
+                                    //scrollValue(what, event)
+                                }
+                                , 500
+                            );
+
+                        }
+                        //
+                    }
+                    else {
+                        filterContent.css('position','fixed').css('top','-'+var_topFilterContent+'px').css('bottom','auto');
+                        innerFilter.css('height', var_heightWindow +'px');
+                        if(!$('#scroll-pane').data('jsp')){
+                            $('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
+                        }
+                        if(!_jScrollNonBottomInitted){
+                            _jScrollNonBottomInitted = true;
+                            $('#scroll-pane').jScrollPane({contentWidth: innerFilter.width()});
+                            //$('#scroll-pane').data('jsp').scrollToBottom();
+                        }
+                    }
+
+                }
+                if(needDel){
+                    if($('#scroll-pane').data('jsp')){
+                        $('#scroll-pane').data('jsp').destroy();
+                    }
+                }
+            }
+            else {
+                var _issetMaps = $('#all-hotels-map').length > 0 && $('#all-hotels-map').is(':visible');
+                var _issetLeftBlock = $('.left-block').length > 0 && $('.left-block').is(':visible');
+                if (_issetMaps && ! _issetLeftBlock) {
+
+                }
+                else {
+                    if($('#scroll-pane').data('jsp')){
+                        $('#scroll-pane').data('jsp').destroy();
+                    }
+
+                }
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return;
+    }
 }
 
 function reInitJScrollPane(){
@@ -1479,87 +1484,85 @@ $(function(){
 });
 
 function resizePanel(arg) {
-    if ($('.panelTable').length > 0 && $('.panelTable').is(':visible')) {
 
         console.log('!!!==== 7 ====!!!');
-            $('.panelTable').each(function(index){
+        $('.panelTable').each(function(index){
 
-                var _panelTable = $(this);//$('.panel').last().find('.panelTable');
-                var _classThis;
-                var _midWidth = 1130;
-                var _minWidth = 1000;
-                var _newMean;
+            var _panelTable = $(this);//$('.panel').last().find('.panelTable');
+            var _classThis;
+            var _midWidth = 1130;
+            var _minWidth = 1000;
+            var _newMean;
 
-                var _allWidthPanel, _midWidthPanel, _maxWidthPanel;
-                //_panelTable.find('.tdPeople').css('width', 'auto');
+            var _allWidthPanel, _midWidthPanel, _maxWidthPanel;
+            //_panelTable.find('.tdPeople').css('width', 'auto');
 
-                if (_panelTable.hasClass('avia')) {
-                    _classThis = 'avia';
-                    var _meanPanel = 850;
-                    var _standartData = 290;
-                    var _widthTdTumblr = _panelTable.find('.tdTumblr').innerWidth();
-                    var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
-                    var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
-                    var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();;
-                    var _widthTdAddTour = 0;
-                    var _howManyInput = 2;
-                }  else if (_panelTable.hasClass('constructorTable')) {
-                    _classThis = 'constructorTable';
-                    if ($(this).find('.tdPeople').hasClass('notFinal')) {
-                        $(this).find('.tdPeople.notFinal').css('width', $('.tdPeople.final').width()+'px');
-                    }
-                    else if ($(this).find('.tdPeople').hasClass('final')) {
-                        //$(this).find('.tdPeople.final').css('width', 'auto');
-                    }
-                    var _meanPanel = 692;
-                    var _widthTdTumblr = 0;
-                    var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
-                    var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
-                    var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();
-                    var _widthTdAddTour = _panelTable.find('.tdAddTour').innerWidth();
-                    var _howManyInput = 1;
-                }  else if (_panelTable.hasClass('hotel')) {
-                    _classThis = 'hotel';
-                    var _meanPanel = 692;
-                    var _widthTdTumblr = 0;
-                    var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
-                    var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
-                    var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();
-                    var _widthTdAddTour = 117;
-                    var _howManyInput = 1;
+            if (_panelTable.hasClass('avia')) {
+                _classThis = 'avia';
+                var _meanPanel = 850;
+                var _standartData = 290;
+                var _widthTdTumblr = _panelTable.find('.tdTumblr').innerWidth();
+                var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
+                var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
+                var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();;
+                var _widthTdAddTour = 0;
+                var _howManyInput = 2;
+            }  else if (_panelTable.hasClass('constructorTable')) {
+                _classThis = 'constructorTable';
+                if ($(this).find('.tdPeople').hasClass('notFinal')) {
+                    $(this).find('.tdPeople.notFinal').css('width', $('.tdPeople.final').width()+'px');
                 }
+                else if ($(this).find('.tdPeople').hasClass('final')) {
+                    //$(this).find('.tdPeople.final').css('width', 'auto');
+                }
+                var _meanPanel = 692;
+                var _widthTdTumblr = 0;
+                var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
+                var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
+                var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();
+                var _widthTdAddTour = _panelTable.find('.tdAddTour').innerWidth();
+                var _howManyInput = 1;
+            }  else if (_panelTable.hasClass('hotel')) {
+                _classThis = 'hotel';
+                var _meanPanel = 692;
+                var _widthTdTumblr = 0;
+                var _widthTdPeople = _panelTable.find('.tdPeople').innerWidth();
+                var _widthTdButton = _panelTable.find('.tdButton').innerWidth();
+                var _widthTdCityStart = _panelTable.find('.tdCityStart').innerWidth();
+                var _widthTdAddTour = 117;
+                var _howManyInput = 1;
+            }
 
-                var _windowWidth = $(window).width();
-                var _widthPanelTable = _panelTable.innerWidth();
+            var _windowWidth = $(window).width();
+            var _widthPanelTable = _panelTable.innerWidth();
 
-                var _dataDiv = _panelTable.find('.tdCity').find('.data');
-                var _dataInput = _panelTable.find('.tdCity').find('.data').find('input');
+            var _dataDiv = _panelTable.find('.tdCity').find('.data');
+            var _dataInput = _panelTable.find('.tdCity').find('.data').find('input');
 
-                if (_windowWidth <= _midWidth && _windowWidth >= _minWidth) {
-                    _allWidthPanel = _windowWidth - 230;
-                }
-                else if (_windowWidth < _minWidth) {
-                    _allWidthPanel = 1000 - 230;
-                }
-                else {
-                    _allWidthPanel = 900;
-                }
+            if (_windowWidth <= _midWidth && _windowWidth >= _minWidth) {
+                _allWidthPanel = _windowWidth - 230;
+            }
+            else if (_windowWidth < _minWidth) {
+                _allWidthPanel = 1000 - 230;
+            }
+            else {
+                _allWidthPanel = 900;
+            }
 
-                if (_widthPanelTable >= _allWidthPanel) {
-                    _newMean = (_allWidthPanel - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
-                }
-                else if (_widthPanelTable < _allWidthPanel) {
-                    _newMean = (_allWidthPanel - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
-                }
-                else {
-                    _newMean = (_widthPanelTable - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
-                }
+            if (_widthPanelTable >= _allWidthPanel) {
+                _newMean = (_allWidthPanel - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
+            }
+            else if (_widthPanelTable < _allWidthPanel) {
+                _newMean = (_allWidthPanel - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
+            }
+            else {
+                _newMean = (_widthPanelTable - _widthTdTumblr - _widthTdPeople - _widthTdButton - _widthTdCityStart - _widthTdAddTour) / _howManyInput;
+            }
 
-                _panelTable.find('.tdCity').find('.data').css('width', _newMean +'px');
-                _panelTable.find('.tdCity').find('.data').find('input').css('width', (_newMean-20) +'px');
-                _widthPanelTable = _panelTable.innerWidth();
-            });
-    }
+            _panelTable.find('.tdCity').find('.data').css('width', _newMean +'px');
+            _panelTable.find('.tdCity').find('.data').find('input').css('width', (_newMean-20) +'px');
+            _widthPanelTable = _panelTable.innerWidth();
+        });
 }
 
 function startIE() {
