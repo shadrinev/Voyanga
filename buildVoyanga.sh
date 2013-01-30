46f9d5ed002dd4f78756c7b3e2e6d537bb92ef10
mihan007 <mihan007@ya.ru>
2013-01-30 18:05:40 +0400

    fix for makefile

--- buildVoyanga.sh
+++ buildVoyanga.sh
@@ -2,5 +2,5 @@ rm Makefile
 rm -rf ./frontend/www/assets
 mkdir ./frontend/www/assets
 chmod 777 ./frontend/www/assets
-curl -L http://frontend.voyanga.lcl/deploy.php
+curl -L http://voyanga.com/deploy.php
 make
