1ef3e754374c54604ca675dce4660b93173575e4
mihan007 <mihan007@ya.ru>
2013-02-01 14:06:07 +0400

    minimizer updater

--- buildVoyanga.sh
+++ buildVoyanga.sh
@@ -2,5 +2,5 @@ rm -f Makefile
 rm -rf ./frontend/www/assets
 mkdir ./frontend/www/assets
 chmod 777 ./frontend/www/assets
-curl -L http://voyanga.com/site/deploy/key/kasdjnfkn24r2wrn2efk#avia
+curl -u voyanga:rabotakipit -L http://voyanga.com/site/deploy/key/kasdjnfkn24r2wrn2efk
 make
