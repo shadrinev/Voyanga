867727bd38bd2d141bf1d81e137fd9872290a407
mihan007 <mihan007@ya.ru>
2013-02-05 19:26:52 +0400

    disable logs

--- common/config/environments/params-prod.php
+++ common/config/environments/params-prod.php
@@ -8,8 +8,8 @@
 return array(
 	"env.code" => "prod",
 
-    'enableHotelLogging' => true,
-    'enableFlightLogging' => true,
+    'enableHotelLogging' => false,
+    'enableFlightLogging' => false,
 
     "api.endPoint" => "http://api.voyanga.com/v1/",
     'app.api.flightSearchUrl' => 'http://api.voyanga.com/v1/flight/search/BE',
