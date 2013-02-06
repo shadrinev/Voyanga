95c601f8d8f887ea45f22e2195394f9d2b641b17
mihan007 <mihan007@ya.ru>
2013-02-06 14:23:41 +0400

    raven.js sends exceptions about js to sentry now

--- frontend/assets/v2/js/compiled/app/app.js
+++ frontend/assets/v2/js/compiled/app/app.js
@@ -40,8 +40,15 @@ Application = (function(_super) {
 
     this.initCalendar = __bind(this.initCalendar, this);
 
-    var result,
+    var result, _oldOnerrorHandler,
       _this = this;
+    _oldOnerrorHandler = window.onerror;
+    window.onerror = function(error) {
+      new ErrorPopup('e500withText', [error]);
+      if (_oldOnerrorHandler) {
+        return _oldOnerrorHandler.apply(_this, arguments_);
+      }
+    };
     this.activeModule = ko.observable(null);
     this.activeModuleInstance = ko.observable(null);
     this.activeSearchPanel = ko.observable(null);
