5cd377443551e4a861dc4f4546ee01b890b9bb61
Anatoly Kudinov <zz@rialabs.org>
2013-02-13 16:37:17 +0400

    RM-679. Определение таймзоны пользователя. Для кореектного отображения таймзон нейтрального времени.

--- frontend/assets/v2/js/compiled/avia/models.js
+++ frontend/assets/v2/js/compiled/avia/models.js
@@ -8,8 +8,8 @@ FlightPart = (function() {
 
   function FlightPart(part) {
     this.part = part;
-    this.departureDate = new Date(part.datetimeBegin + '+04:00');
-    this.arrivalDate = new Date(part.datetimeEnd + '+04:00');
+    this.departureDate = new Date(part.datetimeBegin + Utils.tzOffset);
+    this.arrivalDate = new Date(part.datetimeEnd + Utils.tzOffset);
     this.departureCity = part.departureCity;
     this.departureCityPre = part.departureCityPre;
     this.departureAirport = part.departureAirport;
@@ -83,7 +83,7 @@ Voyage = (function() {
         }
       }
     }
-    this.departureDate = new Date(flight.departureDate + '+04:00');
+    this.departureDate = new Date(flight.departureDate + Utils.tzOffset);
     this.arrivalDate = new Date(this.parts[this.parts.length - 1].arrivalDate);
     this._duration = flight.fullDuration;
     this.departureAirport = this.parts[0].departureAirport;
@@ -845,13 +845,13 @@ AviaResultSet = (function() {
     this.rawSP = sp;
     this.arrivalCity = sp.destinations[0].arrival;
     this.departureCity = sp.destinations[0].departure;
-    this.rawDate = moment(new Date(sp.destinations[0].date + '+04:00'));
-    this.date = dateUtils.formatDayShortMonth(new Date(sp.destinations[0].date + '+04:00'));
+    this.rawDate = moment(new Date(sp.destinations[0].date + Utils.tzOffset));
+    this.date = dateUtils.formatDayShortMonth(new Date(sp.destinations[0].date + Utils.tzOffset));
     this.dateHeadingText = this.date;
     this.roundTrip = sp.isRoundTrip;
     if (this.roundTrip) {
-      this.rtDate = dateUtils.formatDayShortMonth(new Date(sp.destinations[1].date + '+04:00'));
-      this.rawRtDate = moment(new Date(sp.destinations[1].date + '+04:00'));
+      this.rtDate = dateUtils.formatDayShortMonth(new Date(sp.destinations[1].date + Utils.tzOffset));
+      this.rawRtDate = moment(new Date(sp.destinations[1].date + Utils.rzOffset));
       return this.dateHeadingText += ', ' + this.rtDate;
     }
   };
