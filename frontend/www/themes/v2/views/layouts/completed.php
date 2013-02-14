e7a4bc2dddeaff1385c8f4981c7d789005e00128
Anatoly Kudinov <zz@rialabs.org>
2013-02-14 15:30:31 +0400

    Добавил счетчик метрики

--- frontend/www/themes/v2/views/layouts/completed.php
+++ frontend/www/themes/v2/views/layouts/completed.php
@@ -44,6 +44,57 @@ Yii::app()->clientScript->registerScriptFile('/js/eventPage.js');
   })();
 
 </script>
+<!-- Yandex.Metrika counter -->
+
+<script type="text/javascript">
+
+(function (d, w, c) {
+
+    (w[c] = w[c] || []).push(function() {
+
+        try {
+
+            w.yaCounter20065261 = new Ya.Metrika({id:20065261,
+
+                    webvisor:true,
+
+                    clickmap:true,
+
+                    trackLinks:true,
+
+                    accurateTrackBounce:true});
+
+        } catch(e) { }
+
+    });
+
+ 
+
+    var n = d.getElementsByTagName("script")[0],
+
+        s = d.createElement("script"),
+
+        f = function () { n.parentNode.insertBefore(s, n); };
+
+    s.type = "text/javascript";
+
+    s.async = true;
+
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+ 
+
+    if (w.opera == "[object Opera]") {
+
+        d.addEventListener("DOMContentLoaded", f, false);
+
+    } else { f(); }
+
+})(document, window, "yandex_metrika_callbacks");
+
+</script>
+
+<noscript><div><img src="//mc.yandex.ru/watch/20065261" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <script type="text/javascript">
     $(function(){
         Raven.config('<?php echo Yii::app()->params['sentry.dsn']; ?>').install()
--- frontend/www/themes/v2/views/layouts/main.php
+++ frontend/www/themes/v2/views/layouts/main.php
@@ -42,6 +42,57 @@ Yii::app()->clientScript->registerScriptFile('/js/runApp.js');
   })();
 
 </script>
+<!-- Yandex.Metrika counter -->
+
+<script type="text/javascript">
+
+(function (d, w, c) {
+
+    (w[c] = w[c] || []).push(function() {
+
+        try {
+
+            w.yaCounter20065261 = new Ya.Metrika({id:20065261,
+
+                    webvisor:true,
+
+                    clickmap:true,
+
+                    trackLinks:true,
+
+                    accurateTrackBounce:true});
+
+        } catch(e) { }
+
+    });
+
+ 
+
+    var n = d.getElementsByTagName("script")[0],
+
+        s = d.createElement("script"),
+
+        f = function () { n.parentNode.insertBefore(s, n); };
+
+    s.type = "text/javascript";
+
+    s.async = true;
+
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+ 
+
+    if (w.opera == "[object Opera]") {
+
+        d.addEventListener("DOMContentLoaded", f, false);
+
+    } else { f(); }
+
+})(document, window, "yandex_metrika_callbacks");
+
+</script>
+
+<noscript><div><img src="//mc.yandex.ru/watch/20065261" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <script type="text/javascript">
     window.currentCityCode = '<?php echo Geoip::getCurrentCity()->code;?>';
     $(function(){
