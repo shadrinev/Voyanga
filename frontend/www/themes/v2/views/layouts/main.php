2394d07dcccf5c3d329c7881e790bd37de7759fc
Anatoly Kudinov <zz@rialabs.org>
2013-02-14 12:49:11 +0400

    Добавил код гугл аналитики во все лаеуты которые нашел.

--- frontend/www/themes/v2/views/layouts/main.php
+++ frontend/www/themes/v2/views/layouts/main.php
@@ -30,6 +30,19 @@ Yii::app()->clientScript->registerScriptFile('/js/runApp.js');
 </head>
 <body data-bind="css: {fixed: in1}">
 <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38508830-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
     window.currentCityCode = '<?php echo Geoip::getCurrentCity()->code;?>';
     $(function(){
         Raven.config('<?php echo Yii::app()->params['sentry.dsn']; ?>').install()
--- frontend/www/themes/v2/views/layouts/static.php
+++ frontend/www/themes/v2/views/layouts/static.php
@@ -32,6 +32,19 @@ Yii::app()->clientScript->registerScriptFile('/js/eventPage.js');
 </head>
 <body>
 <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38508830-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
     $(function(){
         Raven.config('<?php echo Yii::app()->params['sentry.dsn']; ?>').install()
     })
