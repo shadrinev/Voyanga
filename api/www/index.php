b4a281279e1c2922288a86bf61cb5697f7dea583
mihan007 <mihan007@ya.ru>
2013-02-15 16:28:29 +0400

    close tracing errors on api.voyanga

--- api/www/index.php
+++ api/www/index.php
@@ -1,15 +1,19 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
+$debug = true;
+$prodServers = array('api.voyanga.com');
+if ((isset($_SERVER['HTTP_HOST'])) && (in_array($_SERVER['HTTP_HOST'], $prodServers)))
+    $debug = false;
+defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 // On dev display all errors
 if (YII_DEBUG)
 {
     error_reporting(-1);
     ini_set('display_errors', true);
-    header("Access-Control-Allow-Origin: *");
 }
 
 date_default_timezone_set('Europe/Moscow');
+header("Access-Control-Allow-Origin: *");
 
 chdir(dirname(__FILE__) . '/../..');
 
