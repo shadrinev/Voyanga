f51cd9eff74e66c5258ffc34224d45cdbb9200ba
Anatoly Kudinov <zz@rialabs.org>
2013-02-19 17:07:30 +0400

    Счетчики вынесены в общий файл

---
+++ frontend/www/themes/v2/views/layouts/_counters.php
@@ -0,0 +1,83 @@
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38508830-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<!-- Yandex.Metrika counter -->
+
+<script type="text/javascript">
+
+(function (d, w, c) {
+
+    (w[c] = w[c] || []).push(function() {
+
+        try {
+
+            w.yaCounter20065261 = new Ya.Metrika({id:20065261,
+
+                    webvisor:true,
+
+                    clickmap:true,
+
+                    trackLinks:true,
+
+                    accurateTrackBounce:true});
+
+        } catch(e) { }
+
+    });
+
+ 
+
+    var n = d.getElementsByTagName("script")[0],
+
+        s = d.createElement("script"),
+
+        f = function () { n.parentNode.insertBefore(s, n); };
+
+    s.type = "text/javascript";
+
+    s.async = true;
+
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+ 
+
+    if (w.opera == "[object Opera]") {
+
+        d.addEventListener("DOMContentLoaded", f, false);
+
+    } else { f(); }
+
+})(document, window, "yandex_metrika_callbacks");
+
+</script>
+
+<noscript><div><img src="//mc.yandex.ru/watch/20065261" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+<!-- Google Code for &#1058;&#1077;&#1075; &#1088;&#1077;&#1084;&#1072;&#1088;&#1082;&#1077;&#1090;&#1080;&#1085;&#1075;&#1072; -->
+<!-- Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. For instructions on adding this tag and more information on the above requirements, read the setup guide: google.com/ads/remarketingsetup -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 993261133;
+var google_conversion_label = "TCazCJOCqQQQzezP2QM";
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/993261133/?value=0&amp;label=TCazCJOCqQQQzezP2QM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
