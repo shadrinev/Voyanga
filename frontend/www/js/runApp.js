70c07d9235de85f74c8f351749b1b2d1ba00ff6b
mihan007 <mihan007@ya.ru>
2013-03-07 11:24:59 +0400

    выпилил console.log из runApp

--- frontend/www/js/runApp.js
+++ frontend/www/js/runApp.js
@@ -1,6 +1,5 @@
 $(function() {
     var app, avia, hotels, tour;
-    console.time("App dispatching");
     app = new Application();
     avia = new AviaModule();
     hotels = new HotelsModule();
@@ -10,9 +9,6 @@ $(function() {
     app.register('hotels', hotels);
     app.register('avia', avia);
     app.run();
-    console.timeEnd("App dispatching");
-    console.time("Rendering");
     ko.applyBindings(app);
     ko.processAllDeferredBindingUpdates();
-    return console.timeEnd("Rendering");
 });
