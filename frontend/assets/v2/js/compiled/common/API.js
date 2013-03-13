f6bf7bb4fa4af359d9f26f260db969befd962886
mihan007 <mihan007@ya.ru>
2013-03-13 14:28:28 +0400

    апи будет логгировать только ошибки со статусом > 0

--- frontend/assets/v2/js/compiled/common/API.js
+++ frontend/assets/v2/js/compiled/common/API.js
@@ -36,7 +36,9 @@ API = (function() {
         var jqXHR, rest;
         jqXHR = arguments[0], rest = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
         _this.loader.hide();
-        throw new Error(("Api call failed: Url: " + url) + " | Status: " + jqXHR.status + " | Status text '" + jqXHR.statusText + "' | " + jqXHR.getAllResponseHeaders().replace("\n", ";") + " | " + rest.join(" | "));
+        if (jqXHR.status > 0) {
+          throw new Error(("Api call failed: Url: " + url) + " | Status: " + jqXHR.status + " | Status text '" + jqXHR.statusText + "' | " + jqXHR.getAllResponseHeaders().replace("\n", ";") + " | " + rest.join(" | "));
+        }
       }
     });
   };
