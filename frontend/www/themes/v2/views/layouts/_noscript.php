c104aaa7f13018d4c3d879a3d464b4029cbda550
severenit <severenit@gmail.com>
2013-03-13 18:49:35 +0400

    some fix

--- frontend/www/themes/v2/views/layouts/_noscript.php
+++ frontend/www/themes/v2/views/layouts/_noscript.php
@@ -1,1 +1,27 @@
-<div class="noscript">Для работы с сайтом необходим javascript. Включите, пожалуйста, поддержку скриптов в настройках браузера и перезагрузите страницу.</div>
+<div class="noscript" style="display: none">
+        <div id="errorpopup" class="body-popup" style="z-index: 2009; width: 1440px;">
+            <div id="layer" style="width: 1424px;">
+                <div class="pv_cont error" style="padding-top: 256.5px;">
+                    <table cellspacing="0" cellpadding="0">
+                        <tbody>
+                        <tr>
+                            <td>
+                                <div id="pv_box">
+                                    <div class="contentText">
+                                        <div>
+                                            <h1 data-bind="text: data.title">Упс</h1>
+                                            <p data-bind="html: data.text"> Для работы с сайтом необходим javascript. Включите, пожалуйста, поддержку скриптов в настройках браузера и перезагрузите страницу.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="pv_switch"> </div>
+            </div>
+        </div>
+        <div id="popupOverlay" style="z-index:2008"></div>
+</div>
+
