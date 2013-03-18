5a8a19cad9f1b0550486af4dde1712a2fce73ba1
Anatoly Kudinov <zz@rialabs.org>
2013-03-18 14:33:20 +0400

    Вернулись к продакшн шлюзу немо

--- common/config/params.php
+++ common/config/params.php
@@ -31,8 +31,8 @@ return CMap::mergeArray(array(
         'login' => 'webdev012',
         'password' => 'HHFJGYU3*^H',
         'userId' => 15,
-        'agencyWsdlUri' => 'http://test.nemo-ibe.com/nemoflights/wsdl.php?for=',
-#        'agencyWsdlUri' => 'http://easytrip.nemo-ibe.com/nemoflights/wsdl.php?for=',
+#        'agencyWsdlUri' => 'http://test.nemo-ibe.com/nemoflights/wsdl.php?for=',
+        'agencyWsdlUri' => 'http://easytrip.nemo-ibe.com/nemoflights/wsdl.php?for=',
         'agencyId' => '120',
         'agencyApiKey' => '85C46C441F08204652F2DFADC3DE05CD'
     ),
