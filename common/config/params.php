e90e7844851c17c2fec34afa31f752f526a13f86
Anatoly Kudinov <zz@rialabs.org>
2013-03-18 21:39:35 +0400

    Ищем на easytrip

--- common/config/params.php
+++ common/config/params.php
@@ -33,7 +33,7 @@ return CMap::mergeArray(array(
         'userId' => 15,
         'apiHost' => 'http://test.nemo-ibe.com',
         'agencyBookingWsdlUri' => 'http://test.nemo-ibe.com/nemoflights/wsdl.php?for=',
-        'agencyWsdlUri' => 'http://test.nemo-ibe.com/nemoflights/wsdl.php?for=',
+        'agencyWsdlUri' => 'http://easytrip.nemo-ibe.com/nemoflights/wsdl.php?for=',
         'agencyId' => '120',
         'agencyApiKey' => '85C46C441F08204652F2DFADC3DE05CD'
     ),
