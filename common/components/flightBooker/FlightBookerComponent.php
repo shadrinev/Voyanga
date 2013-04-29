724b191ab1399844cb0a79ffee64a87d6acea312
Anatoly Kudinov <zz@rialabs.org>
2013-04-29 17:27:13 +0400

    - Фейкинг перовго букинга на тесте

--- common/components/flightBooker/FlightBookerComponent.php
+++ common/components/flightBooker/FlightBookerComponent.php
@@ -103,7 +103,7 @@ class FlightBookerComponent extends CApplicationComponent
 
     public function stageBooking()
     {
-        if($this->book_(true))
+        if($this->book_())
         {
             $this->status('waitingForPayment');
         }
