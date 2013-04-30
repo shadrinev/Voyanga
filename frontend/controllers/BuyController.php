806c091f9b28f84c37eff7995a300f956c2413dd
mihan007 <mihan007@ya.ru>
2013-04-30 19:21:26 +0400

    close connection

--- frontend/controllers/BuyController.php
+++ frontend/controllers/BuyController.php
@@ -111,7 +111,6 @@ class BuyController extends FrontendController
     {
         header("Connection: close", true);
         header("Content-Length: 0", true);
-        ob_end_flush();
         flush();
         fastcgi_finish_request(); // important when using php-fpm!
 
